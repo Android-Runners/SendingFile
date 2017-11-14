5382f5c5a14267aa8a29b24a11fe4ecb909b30d1
me <saveliev@dlit.dp.ua>
2017-11-15 01:37:50 +0200

    1st commit

---
+++ src/Dispatcher.java
@@ -0,0 +1,174 @@
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class Dispatcher {
+
+    public static void main(String[] args) {
+
+        new Thread(new Client()).start();
+    }
+
+    private static class Client implements Runnable {
+
+        private DatagramPacket packet;
+
+        private DatagramSocket socket;
+        private InetAddress address;
+
+        private byte[] buf = new byte[60 * 1024];
+        private int PORT = 50000;
+        private String kek = "127.0.0.1";
+
+        private Scanner scanner;
+
+        int clientNumber;
+
+        @Override
+        public void run() {
+            scanner = new Scanner(System.in);
+
+            try {
+                socket = new DatagramSocket();
+                address = InetAddress.getByName(kek);
+            } catch (SocketException | UnknownHostException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                packet = new DatagramPacket(new byte[] {'0'}, 1, address, PORT);
+                socket.send(packet);
+                socket.receive(packet);
+                clientNumber = packet.getData()[0];
+                System.out.println("Number = " + clientNumber);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            new Thread(new Listener(socket)).start();
+
+            while(true) {
+
+                String path = scanner.nextLine();
+
+                try {
+                    byte j = 0;
+                    FileInputStream fileInputStream = new FileInputStream(path);
+//                    System.out.println("File size = " + fileInputStream.available());
+                    while(fileInputStream.available() > 0) {
+                        if(fileInputStream.available() < buf.length) {
+                            buf = new byte[fileInputStream.available()];
+                        }
+                        fileInputStream.read(buf, 0, buf.length);
+                        buf = concat(new byte[]{(byte) clientNumber, j++}, buf);
+//                        System.out.println("Sent: " + Arrays.toString(buf));
+                        packet = new DatagramPacket(buf, buf.length, address, PORT);
+                        socket.send(packet);
+                        buf = new byte[60 * 1024];
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                // Задержка
+//                for(int i = 0; i < 10000000; ++i);
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    // TODO: string "end"
+                    DatagramPacket datagramPacket = new DatagramPacket(new byte[] {(byte) clientNumber, -1}, 2, address, PORT);
+                    socket.send(datagramPacket);
+//                    System.out.println("Sent: " + Arrays.toString(datagramPacket.getData()));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                buf = new byte[60 * 1024];
+            }
+        }
+
+        private byte[] concat(byte[] a, byte[] b) {
+            byte[] t = new byte[a.length + b.length];
+            System.arraycopy(a, 0, t, 0, a.length);
+            System.arraycopy(b, 0, t, a.length, b.length);
+            return t;
+        }
+
+        class Listener implements Runnable {
+
+            private DatagramSocket socket;
+
+            Listener(DatagramSocket socket) {
+                this.socket = socket;
+            }
+
+            @Override
+            public void run() {
+                while(true) {
+                    try {
+                        receive();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            byte[][] buffers = new byte[256][];
+
+            int buffersCount = 0;
+
+            private void receive() throws IOException {
+                byte[] buf = new byte[60 * 1024];
+                DatagramPacket packet = new DatagramPacket(buf, buf.length);
+//                System.out.println("Receiving");
+                socket.receive(packet);
+//                System.out.println("Received. Size = " + packet.getLength());
+//                System.out.println("Received: " + Arrays.toString(packet.getData()));
+                byte[] lol = new byte[packet.getLength()];
+                System.arraycopy(packet.getData(), 0, lol, 0, packet.getLength());
+//                System.out.println("length " + packet.getLength() + " " + buf.length);
+                if(lol[0] == -1) {
+                    writeFile();
+                    buffersCount = 0;
+                    buffers = new byte[256][];
+                    return;
+                }
+                int k = lol[0];
+                lol = deleteFirstByte(lol);
+                System.out.println("k = " + k);
+                buffers[k] = lol;
+                ++buffersCount;
+            }
+
+            private byte[] deleteFirstByte(byte[] s) {
+                byte[] ans = new byte[s.length - 1];
+                System.arraycopy(s, 1, ans, 0, s.length - 1);
+                return ans;
+            }
+
+            private synchronized void writeFile() throws IOException {
+//                System.out.println("writing file");
+                File file = new File("D:\\java\\keeek.txt");
+                if(!file.exists()) {
+                    try {
+                        file.createNewFile();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                System.out.println("buffersCount = " + buffersCount);
+
+                FileOutputStream fileOutputStream = new FileOutputStream("D:\\java\\keeek.mp4");
+                for(int i = 0; i < buffersCount; ++i) {
+                    System.out.println(i + ": " + buffers[i]);
+                    fileOutputStream.write(buffers[i]);
+                }
+            }
+        }
+    }
+}
